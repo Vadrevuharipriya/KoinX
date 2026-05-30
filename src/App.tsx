68a3b735c4d4c2c5e23d9cb79d121e5ba1665dd3
Hari Priya Vadrevu <vadrevu.haripriya.mec23@itbhu.ac.in>
2026-05-30 17:45:43 +0530

    changed path of logo

--- src/App.tsx
+++ src/App.tsx
@@ -151,7 +151,11 @@ export default function App() {
       >
         
         <div style={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-          <img src="assets/logo.png" alt="KoinX Logo" style={{ width: 96, height: 24 }} />
+          <img
+            src="/logo.png"
+            alt="KoinX Logo"
+            style={{ width: 96, height: 24 }}
+          />
         </div>
       </nav>
 
